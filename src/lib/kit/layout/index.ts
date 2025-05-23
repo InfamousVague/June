38f89ce4f146f34c090f742318f2f8e49dbfa3aa
InfamousVagueRat <hey@matt.sucks>
2025-05-23 13:29:39 -0400

    fix(Toolbar): Fix export on toolbar layout

--- src/lib/kit/layout/index.ts
+++ src/lib/kit/layout/index.ts
@@ -1,3 +1,4 @@
 export { default as Accordion } from "./accordion/Accordion.svelte";
 export { default as Card } from "./card/Card.svelte";
 export { default as Sidebar } from "./sidebar/Sidebar.svelte";
+export { default as Toolbar} from "./toolbar/Toolbar.svelte";
